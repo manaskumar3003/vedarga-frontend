25e50353ecf5f559a98e05c9f0eb6dd2e0f46e2f
Manas Kumar <141910018+manaskumar3003@users.noreply.github.com>
2024-07-15 02:07:00 +0530

    fixed

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,11 +2,17 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import Blogtemplate from './blogtemplate';
-import { title } from 'process';
+
+interface BlogContentType {
+    _id: string;
+    title: string;
+    content: string;
+  }
+
 
 
 const page = ({ params }: { params: { slug: string } }) => {
-    let [BlogContent, setBlogContent] = useState([]);
+    let [BlogContent, setBlogContent] = useState<BlogContentType | null>(null);
 
     useEffect(() => {
         axios.get("http://localhost:5000/blog/bloglist")
